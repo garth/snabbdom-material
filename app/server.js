4b47734c87e874a8b67ff6b8a4044f2294b7f65c
Garth Williams <garth@wi.llia.ms>
2016-01-02 15:41:21 +0100

    Setup eslint, webpack and babel

---
+++ app/server.js
@@ -0,0 +1,40 @@
+/* eslint no-console: 0 */
+import path from 'path';
+import fs from 'fs';
+import express from 'express';
+import webpack from 'webpack';
+import webpackMiddleware from 'webpack-dev-middleware';
+import webpackHotMiddleware from 'webpack-hot-middleware';
+import config from '../webpack.config.js';
+
+const port = 3000;
+const app = express();
+
+// compile assets and serve from webpack
+const compiler = webpack(config);
+const middleware = webpackMiddleware(compiler, {
+  publicPath: config.output.publicPath,
+  contentBase: 'src',
+  stats: {
+    colors: true,
+    hash: false,
+    timings: true,
+    chunks: false,
+    chunkModules: false,
+    modules: false
+  }
+});
+app.use(middleware);
+app.use(webpackHotMiddleware(compiler));
+app.get('*', function response(req, res) {
+  res.write(middleware.fileSystem.readFileSync(path.join(__dirname, '../dist/index.html')));
+  res.end();
+});
+
+// start the server
+app.listen(port, '0.0.0.0', function onStart(err) {
+  if (err) {
+    console.log(err);
+  }
+  console.info('==> Listening on port %s. Open up http://0.0.0.0:%s/ in your browser.', port, port);
+});
