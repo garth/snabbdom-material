3d5f531fd61c6eb0fca450e18925853ec301d86c
Garth Williams <garth@wi.llia.ms>
2016-01-29 10:53:12 +0100

    Fix the routing

--- app/router.js
+++ app/router.js
@@ -1,28 +1,30 @@
 import router from 'cerebral-module-router'
 import controller from './controller'
 
-controller.addModules(router({
-  '/': 'introductionPageOpened',
-  '/gettingStarted': 'gettingStartedPageOpened',
-  '/paper': 'paperPageOpened',
-  '/typography': 'typographyPageOpened',
-  '/appbar': 'appbarPageOpened',
-  '/sidenav': 'sidenavPageOpened',
-  '/form': 'formPageOpened',
-  '/button': 'buttonPageOpened',
-  '/input': 'inputPageOpened',
-  '/select': 'selectPageOpened',
-  '/checkbox': 'checkboxPageOpened',
-  '/gridSystem': 'gridSystemPageOpened',
-  '/icon': 'iconPageOpened',
-  '/menu': 'menuPageOpened',
-  '/dialog': 'dialogPageOpened',
-  '/datePicker': 'datePickerPageOpened',
-  '/table': 'tablePageOpened',
-  '/spinner': 'spinnerPageOpened',
-  '/calendar': 'calendarPageOpened',
-  '/responsiveEvents': 'responsiveEventsPageOpened',
-  '/*': 'notFoundPageOpened'
-}, {
-  onlyHash: true
-}))
+controller.addModules({
+  router: router({
+    '/': 'introductionPageOpened',
+    '/gettingStarted': 'gettingStartedPageOpened',
+    '/paper': 'paperPageOpened',
+    '/typography': 'typographyPageOpened',
+    '/appbar': 'appbarPageOpened',
+    '/sidenav': 'sidenavPageOpened',
+    '/form': 'formPageOpened',
+    '/button': 'buttonPageOpened',
+    '/input': 'inputPageOpened',
+    '/select': 'selectPageOpened',
+    '/checkbox': 'checkboxPageOpened',
+    '/gridSystem': 'gridSystemPageOpened',
+    '/icon': 'iconPageOpened',
+    '/menu': 'menuPageOpened',
+    '/dialog': 'dialogPageOpened',
+    '/datePicker': 'datePickerPageOpened',
+    '/table': 'tablePageOpened',
+    '/spinner': 'spinnerPageOpened',
+    '/calendar': 'calendarPageOpened',
+    '/responsiveEvents': 'responsiveEventsPageOpened',
+    '/*': 'notFoundPageOpened'
+  }, {
+    onlyHash: true
+  })
+})
