6e48d9e4f76afc2a92ad2fd1853364c3be1e54ee
Garth Williams <garth@wi.llia.ms>
2016-02-24 02:12:16 -0800

    Auto setup stylingimport variables from '../lib/style/variables'

--- app/components/gettingStarted.js
+++ app/components/gettingStarted.js
@@ -3,25 +3,9 @@ import { Typ } from '../../lib'
 import Example from './example'
 const N = '\n'
 
-export default Component(({
-  props: {
-    material
-  }
-}) => (
+export default Component(() => (
   <div>
-    <Typ headline material={material}>Import Require CSS</Typ>
-    <p>
-      snabbdom-material depends on normalize.css, also some styles and transitions are defined in css and
-      must be included in your project. With webpack simply use the <code>css-loader</code> plugin and
-      include the following line in your main.js:
-    </p>
-
-    <Example code={`${
-    N}import '!style!css!normalize.css/normalize.css';${
-    N}import '!style!css!snabbdom-material/lib/index.css';
-    `}/>
-
-    <Typ headline material={material}>Screen Info</Typ>
+    <Typ headline>Screen Info</Typ>
     <p>
       Many components require a <code>screenInfo</code> object to be passed in via props.
       A <code>getScreenInfo()</code> helper has been provided that will get the required object. However
@@ -34,53 +18,23 @@ export default Component(({
     N}const screenInfo = getScreenInfo();
     `}/>
 
-    <Typ headline material={material}>Custom Styling</Typ>
+    <Typ headline>Custom Styling</Typ>
     <p>
-      All snabbdom-material components accept a <code>material</code> prop which can be used to cutomise
-      the style of the components. Below you can see the default style, which you can copy and modify to
-      customise the look of your components. For color choices please
-      see <a href='https://www.google.com/design/spec/style/color.html#color-color-palette'>
-        https://www.google.com/design/spec/style/color.html#color-color-palette
-      </a>.
+      All snabbdom-material components accept a <code>style</code> prop which can be used to cutomise
+      the style of individual components.
+    </p>
+    <p>
+      To change the global styles add the following to your app init logic:
     </p>
 
     <Example code={`${
-    N}const material = {${
-    N}  primaryColor: '#FFC107',${
-    N}  primaryFontColor: 'rgba(0, 0, 0, 0.7)',${
-    N}  primaryFontColorDisabled: 'rgba(0, 0, 0, 0.45)',${
-    N}  primaryLightWaves: false,${
-    N}  secondaryColor: '#009688',${
-    N}  secondaryFontColor: 'rgba(255, 255, 255, 0.9)',${
-    N}  secondaryFontColorDisabled: 'rgba(255, 255, 255, 0.6)',${
-    N}  secondaryLightWaves: true,${
-    N}  errorColor: '#C00',${
-    N}  successColor: '#090',${
-    N}  typographyColor: '#212121',${
+    N}import variables from 'snabbdom-material/variables'${
+    N}import defaultStyle from 'snabbdom-material/default'${
+    N}import { initStyle } from 'snabbdom-material/style'${
     N}${
-    N}  sidenav: {${
-    N}    width: '280px',${
-    N}    left: '-290px',${
-    N}    transition: 'left .3s ease-out',${
-    N}    delayed: {${
-    N}      left: '0'${
-    N}    },${
-    N}    remove: {${
-    N}      left: '-290px'${
-    N}    }${
-    N}  },${
+    N}// modify the variables or defaultStyle here${
     N}${
-    N}  fadeInOut: {${
-    N}    opacity: '0',${
-    N}    transition: 'opacity .3s',${
-    N}    delayed: {${
-    N}      opacity: '1'${
-    N}    },${
-    N}    remove: {${
-    N}      opacity: '0'${
-    N}    }${
-    N}  }${
-    N}};
+    N}initStyle(defaultStyle(variables))
     `}/>
 
   </div>
--- src/style/index.js
+++ src/style/index.js
@@ -3,6 +3,9 @@ import insert from 'insert-css'
 import easyStyle from 'easy-style'
 import getChild from '../helpers/getChild'
 
+import variables from './variables'
+import defaultStyle from './default'
+
 // inject the css in to the page
 import css from './index.scss'
 insert(css, { prepend: true })
@@ -19,6 +22,9 @@ export function initStyle (defaultStyle, appStyle = {}) {
 }
 
 export function getStyle (name, overrides) {
+  if (!baseStyle) {
+    initStyle(defaultStyle(variables))
+  }
   return overrides
     ? merge(getChild(baseStyle, name), overrides)
     : getChild(baseStyle, name)
