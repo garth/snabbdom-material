e9ae560de6696edb8397e7a9efd97950a18fc9c8
Garth Williams <garth@wi.llia.ms>
2016-02-29 07:59:17 -0800

    Remove jsx from row and col

--- src/components/row.js
+++ src/components/row.js
@@ -1,15 +1,9 @@
-import { html } from 'snabbdom-jsx' // eslint-disable-line
 import h from 'snabbdom/h'
 
 export default function Row ({
-  className = '',
   style = {}
 }, children = '') {
-  return (
-    <div
-      classNames={`${className} row`}
-      style={style}>
-      {h('span', children)}
-    </div>
-  )
+  return h('div.row', {
+    style
+  }, children)
 }
