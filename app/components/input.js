5c393ce9d18e93c230d676ee859a123be6dc713f
Garth Williams <garth@wi.llia.ms>
2016-03-01 04:22:09 -0800

    Remove material from input demo

--- app/components/input.js
+++ app/components/input.js
@@ -6,9 +6,6 @@ const N = '\n'
 export default Component({
   input: ['demos', 'input']
 }, ({
-  props: {
-    material
-  },
   state: {
     input
   },
@@ -25,8 +22,7 @@ export default Component({
             onChange={(e) => signals.emailChanged.sync({ value: e.target.value })}
             isFocused={input.isEmailFocused1}
             onFocus={(e) => signals.focused({ path: 'demos.input.isEmailFocused1'.split('.') })}
-            onBlur={(e) => signals.blurred({ path: 'demos.input.isEmailFocused1'.split('.') })}
-            material={material}/>
+            onBlur={(e) => signals.blurred({ path: 'demos.input.isEmailFocused1'.split('.') })}/>
         </Col>
         <Col type='md-6'>
           <Input
@@ -36,8 +32,7 @@ export default Component({
             onChange={(e) => signals.passwordChanged.sync({ value: e.target.value })}
             isFocused={input.isPasswordFocused}
             onFocus={(e) => signals.focused({ path: 'demos.input.isPasswordFocused'.split('.') })}
-            onBlur={(e) => signals.blurred({ path: 'demos.input.isPasswordFocused'.split('.') })}
-            material={material}/>
+            onBlur={(e) => signals.blurred({ path: 'demos.input.isPasswordFocused'.split('.') })}/>
         </Col>
       </Row>
       <Row>
@@ -49,8 +44,7 @@ export default Component({
             onChange={(e) => signals.emailChanged.sync({ value: e.target.value })}
             isFocused={input.isEmailFocused2}
             onFocus={(e) => signals.focused({ path: 'demos.input.isEmailFocused2'.split('.') })}
-            onBlur={(e) => signals.blurred({ path: 'demos.input.isEmailFocused2'.split('.') })}
-            material={material}/>
+            onBlur={(e) => signals.blurred({ path: 'demos.input.isEmailFocused2'.split('.') })}/>
         </Col>
         <Col type='md-6'>
           <Input
@@ -61,20 +55,19 @@ export default Component({
             onChange={(e) => signals.emailChanged.sync({ value: e.target.value })}
             isFocused={input.isEmailFocused3}
             onFocus={(e) => signals.focused({ path: 'demos.input.isEmailFocused3'.split('.') })}
-            onBlur={(e) => signals.blurred({ path: 'demos.input.isEmailFocused3'.split('.') })}
-            material={material}/>
+            onBlur={(e) => signals.blurred({ path: 'demos.input.isEmailFocused3'.split('.') })}/>
         </Col>
       </Row>
     </div>
     <Example code={`${
     N}<Input label="Email" value={email} onChange={onEmailChange}${
-    N}  isFocused={isFocused} onFocus={onFocus} onBlur={onBlur} material={material}/>${
+    N}  isFocused={isFocused} onFocus={onFocus} onBlur={onBlur}/>${
     N}<Input label="Password" type="password" value={password} onChange={onPasswordChange}${
-    N}  isFocused={isFocused} onFocus={onFocus} onBlur={onBlur} material={material}/>${
+    N}  isFocused={isFocused} onFocus={onFocus} onBlur={onBlur}/>${
     N}<Input label="Success" value={email} isSuccess onChange={onEmailChange}${
-    N}  isFocused={isFocused} onFocus={onFocus} onBlur={onBlur} material={material}/>${
+    N}  isFocused={isFocused} onFocus={onFocus} onBlur={onBlur}/>${
     N}<Input label="Error" value={email} isError message="fix me" onChange={onEmailChange}${
-    N}  isFocused={isFocused} onFocus={onFocus} onBlur={onBlur} material={material}/>
+    N}  isFocused={isFocused} onFocus={onFocus} onBlur={onBlur}/>
     `}/>
   </div>
 ))
